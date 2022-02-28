54f920815f2393010b03ab4831c029bf296bfcd0
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2022-02-28 15:37:23 +0100

    Añadido proceso de personal

--- src/main/java/com/capgemini/ccsw/teammanager/batch/ldap/GroupLdapRepositoryImpl.java
+++ src/main/java/com/capgemini/ccsw/teammanager/batch/ldap/GroupLdapRepositoryImpl.java
@@ -20,6 +20,47 @@ public class GroupLdapRepositoryImpl implements GroupLdapRepository {
    @Override
    public void moveTemporaryToRealGroup() {
 
+      //Insertamos subgrupos
+      this.jdbcTemplate.update("insert into t_subgroups " + "select group_cn, user_cn from t_members tm where user_cn in (select cn from t_group)");
+
+      //Borramos los miembros que no son personas
+      this.jdbcTemplate.update("delete from t_members where user_cn not in (select username from person)");
+      this.jdbcTemplate.update("delete from t_admins where user_cn not in (select username from person)");
+
+      //Borramos los grupos dinámicos
+      this.jdbcTemplate.update("delete from t_group where group_type = 'Dynamic'");
+
+      //Actualizamos el tipo de miembros que tiene el grupo
+      this.jdbcTemplate.update("update t_group set operation_type = ''");
+      this.jdbcTemplate.update("update t_group set operation_type = concat(operation_type, 'A') " + "where cn in (select distinct group_cn from t_admins)");
+      this.jdbcTemplate.update("update t_group set operation_type = concat(operation_type, 'M') " + "where cn in (select distinct group_cn from t_members)");
+      this.jdbcTemplate.update("update t_group set operation_type = concat(operation_type, 'S') " + "where cn in (select distinct group_cn from t_subgroups)");
+
+      //Borramos los grupos que NO tienen admins o que SOLO tienen admins
+      this.jdbcTemplate.update("delete from t_group where operation_type not like 'A%'");
+      this.jdbcTemplate.update("delete from t_group where operation_type = 'A'");
+
+      //Borramos miembros sin grupo
+      this.jdbcTemplate.update("delete from t_operations");
+      this.jdbcTemplate
+            .update("insert into t_operations(value_1, value_2) " + "select group_cn, user_cn from t_members where group_cn in (select cn from t_group)");
+      this.jdbcTemplate.update("delete from t_members");
+      this.jdbcTemplate.update("insert into t_members select value_1, value_2 from t_operations");
+
+      //Borramos admins sin grupo
+      this.jdbcTemplate.update("delete from t_operations");
+      this.jdbcTemplate
+            .update("insert into t_operations(value_1, value_2) " + "select group_cn, user_cn from t_admins where group_cn in (select cn from t_group)");
+      this.jdbcTemplate.update("delete from t_admins");
+      this.jdbcTemplate.update("insert into t_admins select value_1, value_2 from t_operations");
+
+      //Borramos subgrupos sin grupo
+      this.jdbcTemplate.update("delete from t_operations");
+      this.jdbcTemplate.update(
+            "insert into t_operations(value_1, value_2) " + "select group_cn, group_child_cn from t_subgroups where group_cn in (select cn from t_group)");
+      this.jdbcTemplate.update("delete from t_subgroups");
+      this.jdbcTemplate.update("insert into t_subgroups select value_1, value_2 from t_operations");
+
       //Marcamos grupos para insertar o updatear
       this.jdbcTemplate.update("update t_group set operation_type = null");
       this.jdbcTemplate.update("update t_group set operation_type = 'I' where not exists (select 1 from `group` where external_id = cn)");
